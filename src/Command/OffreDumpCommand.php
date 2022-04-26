f9617685d56be37e2686f7bcd6f2ee51536c602e
jfsenechal <jfsenechal@gmail.com>
2022-04-26 19:12:01 +0200

    dump all data

--- src/Command/OffreDumpCommand.php
+++ src/Command/OffreDumpCommand.php
@@ -3,14 +3,12 @@
 namespace AcMarche\Pivot\Command;
 
 use AcMarche\Pivot\Repository\PivotRemoteRepository;
-use AcMarche\Pivot\Repository\PivotRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Serializer\SerializerInterface;
 
 #[AsCommand(
     name: 'pivot:offre-dump',
@@ -19,8 +17,6 @@ use Symfony\Component\Serializer\SerializerInterface;
 class OffreDumpCommand extends Command
 {
     public function __construct(
-        private SerializerInterface $serializer,
-        private PivotRepository $pivotRepository,
         private PivotRemoteRepository $pivotRemoteRepository,
         string $name = null
     ) {
@@ -30,22 +26,20 @@ class OffreDumpCommand extends Command
     protected function configure(): void
     {
         $this
-            ->addArgument('codeCgt', InputArgument::REQUIRED, 'code cgt');
+            ->addArgument('codeCgt', InputArgument::OPTIONAL, 'code cgt');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io      = new SymfonyStyle($input, $output);
+        $io = new SymfonyStyle($input, $output);
         $codeCgt = $input->getArgument('codeCgt');
 
-        if ( ! $codeCgt) {
-            $io->note(sprintf('Code cgt requis: %s', $codeCgt));
-
-            return Command::FAILURE;
+        if (!$codeCgt) {
+            $resultString = $this->pivotRemoteRepository->query();
+        } else {
+            $resultString = $this->pivotRemoteRepository->offreByCgt($codeCgt);
         }
-        $hotelString = $this->pivotRemoteRepository->offreByCgt($codeCgt);
-        echo $hotelString;
-
+        echo $resultString;
         $io->writeln("");
 
         return Command::SUCCESS;
