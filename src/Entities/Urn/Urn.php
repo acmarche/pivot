f30d3dd5e7fe5794e0011a10ce76dcdd95b0684d
jfsenechal <jfsenechal@gmail.com>
2022-04-27 15:20:18 +0200

    new generate

--- src/Entities/Urn/Urn.php
+++ src/Entities/Urn/Urn.php
@@ -1,12 +1,37 @@
 <?php
 
-namespace AcMarche\Pivot\Urn;
+namespace AcMarche\Pivot\Entities\Urn;
 
 //https://organismes.tourismewallonie.be/doc-pivot-gest/liste-des-types-durn/
+use AcMarche\Pivot\Entities\Label;
+
 class Urn
 {
     public int $id;
-    public string $code;
     public string $urn;
-    public string $libele;
+    public string $code;
+    public int $order;
+    public bool $deprecated = false;
+    public string $type;
+    public string $label;
+    public bool $root = false;
+
+    public function __construct(
+        string $urn,
+        string $code,
+        int $order,
+        bool $deprecated,
+        string $type,
+        string $libele,
+        bool $root
+    ) {
+        $this->urn = $urn;
+        $this->code = $code;
+        $this->order = $order;
+        $this->id = $order;
+        $this->deprecated = $deprecated;
+        $this->type = $type;
+        $this->label = $libele;
+        $this->root = $root;
+    }
 }
