37cf22fdd8c23cd8bfe157a3e284d82570b7ed43
jfsenechal <jfsenechal@gmail.com>
2022-04-27 13:57:30 +0200

    move fields to offer

--- src/Entities/Event/Event.php
+++ src/Entities/Event/Event.php
@@ -3,7 +3,6 @@
 namespace AcMarche\Pivot\Entities\Event;
 
 use AcMarche\Pivot\Entities\Offre\Offre;
-use AcMarche\Pivot\Entities\Specification\SpecData;
 use DateTimeInterface;
 
 class Event extends Offre
@@ -11,14 +10,6 @@ class Event extends Offre
     public DateTimeInterface $dateBegin;
     public DateTimeInterface $dateEnd;
 
-    public bool $active;
-    public string $email;
-    public string $tel;
-
-    public string|SpecData|null $description;
-    public string|SpecData|null $tarif;
-
-    public string $image;
     /**
      * @var DateBeginEnd[]
      */
