90d690fa20d230fcb8d3e05d79d2b3a9c705c27a
jfsenechal <jfsenechal@gmail.com>
2022-04-28 14:49:32 +0200

    add new TypeOffreShort2.php

--- src/Entities/Offre/TypeOffreShort2.php
+++ src/Entities/Offre/TypeOffreShort2.php
@@ -3,9 +3,21 @@
 namespace AcMarche\Pivot\Entities\Offre;
 
 use AcMarche\Pivot\Entities\Label;
+use AcMarche\Pivot\Entities\LabelTrait;
 
 class TypeOffreShort2
 {
+    use LabelTrait;
+
     public int $idTypeOffre;
-    public Label $label;
+   // public Label $label;
+
+   /* public function labelByLanguage(string $language = Label::FR): string
+    {
+        if ($this->label->get($language)) {
+            return $this->label->get($language);
+        }
+
+        return 'title found';
+    }*/
 }
