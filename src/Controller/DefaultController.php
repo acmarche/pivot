3fba95ad7d6711bd754cbcfc89a8f22d1ae50cbe
jfsenechal <jfsenechal@gmail.com>
2022-05-03 14:58:13 +0200

    #[AsController]

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,8 +6,10 @@ use AcMarche\Pivot\Parser\OffreParser;
 use AcMarche\Pivot\Repository\PivotRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[AsController]
 #[Route(path: '/')]
 class DefaultController extends AbstractController
 {
