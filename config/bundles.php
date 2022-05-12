16d09ed1250a87f9710b437a25a5ba557dc8f4fe
jfsenechal <jfsenechal@gmail.com>
2022-05-12 10:15:25 +0200

    add di for bottin, move elastic to bottin

--- config/bundles.php
+++ config/bundles.php
@@ -2,7 +2,8 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    AcMarche\Pivot\AcMarchePivotBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    AcMarche\Pivot\AcMarchePivotBundle::class             => ['all' => true],
+    AcMarche\Bottin\AcMarcheBottinBundle::class           => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class     => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class  => ['all' => true],
 ];
