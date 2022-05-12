a8a94ba66d36a7a031b36676e470a09227c64b2f
jfsenechal <jfsenechal@gmail.com>
2022-05-12 15:27:36 +0200

    mailer sf

--- src/Utils/Mailer.php
+++ src/Utils/Mailer.php
@@ -2,15 +2,38 @@
 
 namespace AcMarche\Pivot\Utils;
 
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
 class Mailer
 {
-    public static function sendError(string $subject, string $message): void
+    public function __construct(private MailerInterface $mailer)
+    {
+    }
+
+    public function sendError(string $subject, string $message): void
+    {
+        if ($message = $this->createMessage($subject, $message)) {
+
+            try {
+                $this->mailer->send($message);
+            } catch (TransportExceptionInterface $e) {
+            }
+        }
+    }
+
+    private function createMessage(string $subject, string $message): ?Email
     {
         $to = $_ENV['WEBMASTER_EMAIL'] ?? null;
         if ($to) {
-            if (function_exists('wp_mail')) {
-                wp_mail($to, $subject, $message);//Todo replace sf mailer
-            }
+            return (new Email())
+                ->from($to)
+                ->to($to)
+                ->subject($subject)
+                ->text($message);
         }
+
+        return null;
     }
 }
