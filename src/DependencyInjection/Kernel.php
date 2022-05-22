28fa2c7da5cb99779f3f663c8877b7cfc3179cbc
jfsenechal <jfsenechal@gmail.com>
2022-05-22 20:45:36 +0200

    add link doc

--- src/DependencyInjection/Kernel.php
+++ src/DependencyInjection/Kernel.php
@@ -5,6 +5,7 @@ namespace AcMarche\Pivot\DependencyInjection;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
+//https://symfony.com/doc/6.1/configuration/micro_kernel_trait.html
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
