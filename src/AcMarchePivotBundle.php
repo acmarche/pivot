2f885ee55028c06d6b340a86bc9455ef61263705
jfsenechal <jfsenechal@gmail.com>
2022-05-30 15:17:13 +0200

    AbstractBundle

--- src/AcMarchePivotBundle.php
+++ src/AcMarchePivotBundle.php
@@ -2,12 +2,28 @@
 
 namespace AcMarche\Pivot;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class AcMarchePivotBundle extends Bundle
+class AcMarchePivotBundle  extends AbstractBundle
 {
     public function getPath(): string
     {
         return \dirname(__DIR__);
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.php');
+    }
+
+    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/packages/twig.php');
+        $container->import('../config/packages/framework.php');
+        $container->import('../config/packages/cache.php');
+        $container->import('../config/packages/monolog.php');
+        $container->import('../config/packages/doctrine.php');
     }
 }
