061e4c6330e40b0dd3875ea000d46ad43d177952
jfsenechal <jfsenechal@gmail.com>
2022-06-02 09:38:27 +0200

    add TwigBundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,6 +2,7 @@
 
 $bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     AcMarche\Pivot\AcMarchePivotBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class     => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class  => ['all' => true],
