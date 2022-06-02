e99b225efceca4ec5cc319f0f48e25fe9f443d80
jfsenechal <jfsenechal@gmail.com>
2022-06-02 09:09:17 +0200

    add liip

--- config/bundles.php
+++ config/bundles.php
@@ -4,7 +4,8 @@ $bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     AcMarche\Pivot\AcMarchePivotBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class     => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class  => ['all' => true]
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class  => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
 
 if (class_exists(AcMarche\Bottin\AcMarcheBottinBundle::class)) {
