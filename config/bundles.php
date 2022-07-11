a45d828d8f4fb34ff63b55754de31afabddee679
jfsenechal <jfsenechal@gmail.com>
2022-07-11 17:15:25 +0200

    if class exist

--- config/bundles.php
+++ config/bundles.php
@@ -2,13 +2,16 @@
 
 $bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     AcMarche\Pivot\AcMarchePivotBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class     => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class  => ['all' => true],
-    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
-
+if (class_exists(Symfony\Bundle\TwigBundle\TwigBundle::class)) {
+    $bundles[Symfony\Bundle\TwigBundle\TwigBundle::class] =  ['all' => true];
+}
+if (class_exists(Liip\ImagineBundle\LiipImagineBundle::class)) {
+    $bundles[Liip\ImagineBundle\LiipImagineBundle::class] =  ['all' => true];
+}
 if (class_exists(AcMarche\Bottin\AcMarcheBottinBundle::class)) {
     $bundles[AcMarche\Bottin\AcMarcheBottinBundle::class] =  ['all' => true];
 }
