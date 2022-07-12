58a3f2310b319195c4ef831d6950089fe35d45d0
jfsenechal <jfsenechal@gmail.com>
2022-07-12 12:26:13 +0200

    add urn

--- src/Entities/Category.php
+++ src/Entities/Category.php
@@ -6,14 +6,16 @@ class Category
 {
     use LabelTrait;
 
-    public int $id;
+    public int $id;//order
+    public string $urn;
     /**
      * use for wp
      */
     public string $nom;
 
-    public function __construct(int $id, array $label)
+    public function __construct(string $urn, int $id, array $label)
     {
+        $this->urn = $urn;
         $this->id = $id;
         $this->label = $label;
     }
