3935bc7eb95227f5034a10a4bc8ae5192a0be029
jfsenechal <jfsenechal@gmail.com>
2022-07-14 14:32:41 +0200

    list offre by urn

--- src/Controller/OffreController.php
+++ src/Controller/OffreController.php
@@ -2,9 +2,10 @@
 
 namespace AcMarche\Pivot\Controller;
 
-use AcMarche\Pivot\Parser\OffreParser;
-use AcMarche\Pivot\Repository\PivotRemoteRepository;
+use AcMarche\Pivot\Entity\TypeOffre;
 use AcMarche\Pivot\Repository\PivotRepository;
+use AcMarche\Pivot\Repository\TypeOffreRepository;
+use AcMarche\Pivot\Utils\SortUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
@@ -16,22 +17,25 @@ class OffreController extends AbstractController
 {
     public function __construct(
         private PivotRepository $pivotRepository,
-        private PivotRemoteRepository $pivotRemoteRepository,
-        private OffreParser $pivotParser
+        private TypeOffreRepository $typeOffreRepository,
     ) {
     }
 
-    #[Route(path: '/{urn}', name: 'pivot_offres')]
-    public function index(string $urn): Response
+    #[Route(path: '/{id}/{urn}', name: 'pivot_offres')]
+    public function index(TypeOffre $typeOffre, string $urn): Response
     {
+        $typeOffreUrn = $this->typeOffreRepository->findByUrn($urn);
+        $offres = $this->pivotRepository->getOffres([$typeOffre]);
+        $offres = SortUtils::sortOffres($offres);
 
         return $this->render(
-            '@AcMarchePivot/default/index.html.twig',
+            '@AcMarchePivot/offres/index.html.twig',
             [
-
+                'urn' => $urn,
+                'typeOffre' => $typeOffre,
+                'typeOffreUrn' => $typeOffreUrn,
+                'offres' => $offres,
             ]
         );
     }
-
-
 }
