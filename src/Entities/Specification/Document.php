da82f966416bc47077a435d5eb7212ee5d595d0c
jfsenechal <jfsenechal@gmail.com>
2022-10-26 16:20:27 +0200

    add classes

---
+++ src/Entities/Specification/Document.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AcMarche\Pivot\Entities\Specification;
+
+class Document
+{
+    public ?string $name = null;
+    public ?string $desc = null;
+    public ?string $url = null;
+    public ?string $extension = null;
+    public array|null $link = [];
+}
---
+++ src/Entities/Specification/Gpx.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AcMarche\Pivot\Entities\Specification;
+
+class Gpx
+{
+    public ?string $name = null;
+    public ?string $desc = null;
+    public array|null $links = [];
+    public ?string $url = null;
+    public ?string $data_raw= null;
+}
