9c3aa48f22cf70a1b1fa8b6132ea395db969ae53
jfsenechal <jfsenechal@gmail.com>
2022-11-04 15:34:46 +0100

    filtre tout

--- config/packages/twig.php
+++ config/packages/twig.php
@@ -4,5 +4,6 @@ use Symfony\Config\TwigConfig;
 
 return static function (TwigConfig $twig) {
     $twig
-        ->path('%kernel.project_dir%/src/AcMarche/Pivot/templates', 'AcMarchePivot');
+        ->path('%kernel.project_dir%/src/AcMarche/Pivot/templates', 'AcMarchePivot')
+        ->path('%kernel.project_dir%/wp-content/themes/visittail/templates', 'VisitTail');
 };
