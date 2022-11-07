8c70ec60f226f1fd9e5fb0c6118a2da080287b7e
jfsenechal <jfsenechal@gmail.com>
2022-11-07 22:49:53 +0100

    clean

--- config/packages/twig.php
+++ config/packages/twig.php
@@ -4,6 +4,5 @@ use Symfony\Config\TwigConfig;
 
 return static function (TwigConfig $twig) {
     $twig
-        ->path('%kernel.project_dir%/src/AcMarche/Pivot/templates', 'AcMarchePivot')
-        ->path('%kernel.project_dir%/wp-content/themes/visittail/templates', 'VisitTail');
+        ->path('%kernel.project_dir%/src/AcMarche/Pivot/templates', 'AcMarchePivot');
 };
