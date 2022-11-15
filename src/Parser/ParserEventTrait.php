366b7f5bd748be37e30d1bbb9cb0bfc6fe795c9e
jfsenechal <jfsenechal@gmail.com>
2022-11-15 21:21:32 +0100

    add offre param to functions

--- src/Parser/ParserEventTrait.php
+++ src/Parser/ParserEventTrait.php
@@ -3,22 +3,23 @@
 namespace AcMarche\Pivot\Parser;
 
 use AcMarche\Pivot\Entities\Event\DateBeginEnd;
+use AcMarche\Pivot\Entities\Offre\Offre;
 use AcMarche\Pivot\Spec\UrnList;
 use AcMarche\Pivot\Utils\DateUtils;
 use AcMarche\Pivot\Utils\SortUtils;
 
 trait ParserEventTrait
 {
-    public function dateBeginAndEnd(): array
+    public function dateBeginAndEnd(Offre $offre): array
     {
         $dates = [];
         $format = "d/m/Y";
-        $dateDebut = $this->findByUrn(UrnList::DATE_DEB_VALID->value);
+        $dateDebut = $this->findByUrn($offre, UrnList::DATE_DEB_VALID->value, returnData: true);
         if (count($dateDebut) > 0) {
             $dates[] = DateUtils::convertStringToDateTime($dateDebut[0]->value, $format);
         }
 
-        $dateFin = $this->findByUrn(UrnList::DATE_FIN_VALID->value);
+        $dateFin = $this->findByUrn($offre, UrnList::DATE_FIN_VALID->value, returnData: true);
         if (count($dateFin) > 0) {
             $dates[] = DateUtils::convertStringToDateTime($dateFin[0]->value, $format);
         }
@@ -29,10 +30,10 @@ trait ParserEventTrait
     /**
      * @return DateBeginEnd[]
      */
-    public function getDates(): array
+    public function getDates(Offre $offre): array
     {
         $dates = [];
-        $specs = $this->findByUrn(UrnList::DATE_OBJECT->value);
+        $specs = $this->findByUrn($offre, UrnList::DATE_OBJECT->value, returnData: true);
         foreach ($specs as $spec) {
             foreach ($spec->spec as $data) {
                 if ($data->urn == UrnList::DATE_DEB->value) {
