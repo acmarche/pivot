f1f533b896faf7af6f9de99353c5f92c9b948007
jfsenechal <jfsenechal@gmail.com>
2022-11-16 16:55:29 +0100

    fix description

--- src/Spec/UrnCatList.php
+++ src/Spec/UrnCatList.php
@@ -9,6 +9,7 @@ enum UrnCatList: string
 {
     case COMMUNICATION = "urn:cat:moycom";
     case DESCRIPTION = "urn:cat:descmarket";
+    case DESCRIPTION_30 = "urn:cat:descmarket30";
     case DESCRIPTION_MARKETING = "urn:obj:descmarkettgt";
     case EQUIPEMENTS = "urn:cat:eqpsrv";
     case ACCUEIL = "urn:cat:accueil";
