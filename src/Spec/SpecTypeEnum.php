e6d96a3122988620277537f076858e82e9741368
jfsenechal <jfsenechal@gmail.com>
2022-11-16 14:40:22 +0100

    add vars

--- src/Spec/SpecTypeEnum.php
+++ src/Spec/SpecTypeEnum.php
@@ -4,12 +4,17 @@ namespace AcMarche\Pivot\Spec;
 
 enum SpecTypeEnum: string
 {
-    case TEL = "Phone";
     case EMAIL = "EMail";
     case URL = "Url";
+    case URL_FACEBOOK = "URLFacebook";
+    case URL_TRIPADVISOR = "'URLTripadvisor'";
     case STRING = "String";
+    case STRINGML = "StringML";
     case BOOLEAN = "Boolean";
     case DATE = "Date";
     case OBJECT = "Object";
     case TEXTML = "TextML";
+    case CURRENCY = "Currency";
+    case PHONE = "Phone";
+    case GSM = "GSM";
 }
